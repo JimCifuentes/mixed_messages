e478fab73708618be130ae93fa8871b473cb5c3a
Jim <2014ocm3@gmail.com>
2022-06-01 15:40:47 -0600

    added second set of quotes

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//first set of Inspirational quotes
+const inspirational1 = [`1. “When you have a dream, you’ve got to grab it and never let go.”
+— Carol Burnett`,
+
+`2. “Nothing is impossible. The word itself says ‘I’m possible!'”
+— Audrey Hepburn`,
+
+`3. “There is nothing impossible to they who will try.”
+— Alexander the Great`,
+
+`4. “The bad news is time flies. The good news is you’re the pilot.”
+— Michael Altshuler`,
+
+`5. “Life has got all those twists and turns. You’ve got to hold on tight and off you go.”
+— Nicole Kidman`]
+console.log(inspirational1)
+
+// second set of funny quotes.
+
+const funny = [
+    `1. “I’m sick of following my dreams, man. I’m just going to ask where they’re going and hook up with ’em later.”
+    —Mitch Hedberg`,`
+    
+    2. “Gentlemen, you can’t fight in here. This is the war room.”
+    —President Merkin Muffley (Peter Sellers), Dr. Strangelove `,`
+    
+    3. “My mother always used to say: The older you get, the better you get, unless you’re a banana.”
+    —Rose (Betty White), The Golden Girls`,`
+    
+    4. “Halloween is the beginning of the holiday shopping season. That’s for women. The beginning of the holiday shopping season for men is Christmas Eve.”
+    —David Letterman`,`
+    5. “Before you criticize someone, you should walk a mile in their shoes. That way when you criticize them, you are a mile away from them and you have their shoes.”
+—Jack Handey
+    `
+]
+
+console.log(funny)
